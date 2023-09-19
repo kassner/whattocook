8cb64560817e49426d3a36d3688c08f34832b6bd
Rafael Kassner <rafael@kassner.com.br>
2023-09-19 21:00:39 +0200

    frontend: support for mobile and multiple breakpoints

--- src/main/js/app/main.tsx
+++ src/main/js/app/main.tsx
@@ -84,7 +84,7 @@ const App = () => {
 
     if (!recipe) {
         return (
-            <div className="hero min-h-screen">
+            <div className="hero h-full">
                 <div className="hero-content text-center">
                     <div className="max-w-md">
                         <h1 className="text-5xl font-bold">What to cook today?</h1>
@@ -97,50 +97,42 @@ const App = () => {
     }
 
     return (
-        <div className="container mx-auto pt-2">
-            <div className="grid grid-cols-6 gap-2 flex items-stretch">
-                <div className="border-gray-100 shadow rounded-lg bg-white">
-                    <div className="w-full p-2">
-                        <h2 className="mt-0 text-xl pb-2 text-zinc-500">Session history</h2>
-                        {history.map((element : Event) : any => {
-                            return <>
-                                <div className="divider mt-0 mb-0 divider-gray-100" />
-                                <div className="card w-full">
-                                    <div className="card-body p-0 mt-3 leading-4">
-                                        {element.description && <p className="font-lg font-medium">{element.description}</p>}
-                                        <p className="text-zinc-500 font-sm">{element.type}</p>
-                                        <TimeAgo date={element.date} live={true} minPeriod={10} className="text-sm" />
-                                    </div>
-                                </div>
-                            </>
-                        })}
-                    </div>
-                </div>
-                <div className="col-span-5 card w-full border-gray-100 shadow rounded-lg bg-white">
-                    <div className="card-body p-5">
-                        <h1 className="mt-0 text-4xl pb-2 text-zinc-950">{recipe.name}</h1>
-                        <div className="grid-cols-12 grid gap-4">
-                            <div className="col-span-3">
-                                <h2 className="mt-0 text-2xl pb-2 text-zinc-600">Ingredients</h2>
-                                <ul className="pl-0 list-none leading-9">
-                                    {recipe.ingredients.map((ingredient : Ingredient) : any => {
-                                        return <li className="pl-0 flex items-stretch" key={ingredient.id}>
-                                            <button className="btn btn-square btn-outline btn-error btn-sm" onClick={() => removeIngredient(ingredient)}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                            </button>
-                                            <span className="pl-2 name">{ingredient.name}</span>
-                                        </li>;
-                                    })}
-                                </ul>
-                            </div>
-                            <div className="col-span-9 mt-0">
-                                <h2 className="mt-0 text-2xl pb-2 text-zinc-600">Instructions</h2>
-                                <div className="text-justify">{nl2p(recipe.instructions)}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className="flex flex-col-reverse gap-8 lg:flex-row lg:gap-2 xl:gap-4">
+            <div className="lg:w-48" id="history">
+                <h2 className="text-lg text-zinc-600">Session history</h2>
+                <ol className="divide-y divide-zinc-200">
+                    {history.map((element : Event) : any => {
+                        return <li className="w-full leading-4 py-2">
+                            <p className="text-gray-950 font-lg font-medium">{element.description}</p>
+                            <p className="font-sm">{element.type}</p>
+                            <TimeAgo date={element.date} live={true} minPeriod={10} className="text-sm" />
+                        </li>
+                    })}
+                </ol>
             </div>
+            <div className="divider divider-gray-100 lg:divider-horizontal" />
+            <article className="lg:flex-1">
+                <h1>{recipe.name}</h1>
+                <div className="flex flex-col xl:flex-row">
+                    <div className="mb-6 xl:w-64">
+                        <h2>Ingredients</h2>
+                        <ul className="mt-1 pl-0 list-none leading-9">
+                            {recipe.ingredients.map((ingredient : Ingredient) : any => {
+                                return <li className="pl-0 flex items-stretch" key={ingredient.id}>
+                                    <button className="btn btn-square btn-outline btn-error btn-sm" onClick={() => removeIngredient(ingredient)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                    </button>
+                                    <span className="pl-2">{ingredient.name}</span>
+                                </li>;
+                            })}
+                        </ul>
+                    </div>
+                    <div className="xl:flex-1">
+                        <h2>Instructions</h2>
+                        <div className="mt-1 text-justify">{nl2p(recipe.instructions)}</div>
+                    </div>
+                </div>
+            </article>
         </div>
     );
 }
